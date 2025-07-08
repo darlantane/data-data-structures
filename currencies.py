a94a8413fa81f0853760bbd223038e6028c9f73b
darlan tane <darlan.tane@gmail.com>
2025-07-08 12:50:41 +0200

    Data structures

--- currencies.py
+++ currencies.py
@@ -1,7 +1,12 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {}
+RATES = {
+    "USDEUR": 0.85,
+    "GBPEUR": 1.13,
+    "CHFEUR": 0.86,
+    "EURGBP": 0.885
+}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
@@ -9,4 +14,25 @@ def convert(amount, currency):
     currency is a string
     """
 
-    pass  # YOUR CODE HERE
+    value, from_currency = amount
+
+    if from_currency == currency:
+        return round(value)
+
+    key = from_currency + currency
+    if key in RATES:
+        rate = RATES[key]
+        return round(value * rate)
+
+    inverse_key = currency + from_currency
+    if inverse_key in RATES:
+        rate = RATES[inverse_key]
+        return round(value / rate)
+    else:
+        return None
+
+
+print(convert((100, "USD"), "EUR"))
+print(convert((100, "EUR"), "GBP"))
+print(convert((100, "GBP"), "EUR"))
+print(convert((100, "EUR"), "EUR"))
